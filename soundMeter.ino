0c1416509213582e92209cf038086002edea2cea
amine-12 <54552812+amine-12@users.noreply.github.com>
2020-10-18 18:53:30 -0400

    Second Commit

--- soundMeter.ino
+++ soundMeter.ino
@@ -6,16 +6,11 @@ void setup() {
 }
 void loop() {
   int volume = analogRead(A0);
-  //max value for analog read is 1023 but it must be very very loud to reach this value
-  //so I lower it down in map function to 700
-  //mapping volume value to make it easier to turn LEDs on
   volume = map(volume, 0, 700, 0, 10);
   for(int a = 0; a < 10; a++){  
    if(volume >= a){
-   //if it is bigger we can turn on the LED
     digitalWrite(a, HIGH);
    }else{
-   //if it is smaller we can turn the LED off
     digitalWrite(a, LOW);
    }
 }
